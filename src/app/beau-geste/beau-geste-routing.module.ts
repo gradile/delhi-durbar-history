11a3e872f24501a9ef9d52801f769ca9dba72abf
Graciela Di Lelle <gradile@cox.net>
2026-04-30 23:14:30 -0700

    changes in buttons styles

--- src/app/beau-geste/beau-geste-routing.module.ts
+++ src/app/beau-geste/beau-geste-routing.module.ts
@@ -109,7 +109,7 @@ const routes: Routes = [
   ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class BeauGesteRoutingModule { }
